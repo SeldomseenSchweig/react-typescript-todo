5f23ebd7c03a812d2e4c2af16ba50767f5b5872c
SeldomseenSchweig <jamesschweig@gmail.com>
2024-07-15 17:06:13 -0500

    made status change

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -2,143 +2,130 @@ import React, { useState } from "react";
 import "./todoList.css";
 
 const TodoList = (): any => {
-  const [todoList, setTodoList] = useState<string[]>([]);
+  const [todoList, setTodoList] = useState<
+    { id: number; priority: string; task: string }[]
+  >([]);
   const [inputValue, setInputValue] = useState("");
   const [priority, setPriority] = useState<string>("");
-  const [priorityChange, setNewPriority] = useState("");
-
-  const handleNewPriority = (event: any, id: any) => {
-    setNewPriority(event.target.value);
-    // document.getElementById(`${id}`)?.classList.remove()
-  };
+  const [id, setId] = useState(0);
 
   const handleInputChange = (event: any) => {
     setInputValue(event.target.value);
   };
+
   const handlePriorityChange = (event: any) => {
-    console.log("priortiy change", priority);
     setPriority(event.target.value);
   };
+
   const handleFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     if (inputValue.trim() !== "" && priority.trim() !== "") {
-      setTodoList([...todoList, `${priority}: ${inputValue}`]); // setting todolist
+      const todo = { id, priority, task: inputValue.trim() };
+      setId(id + 1);
+      setTodoList([...todoList, todo]);
       setInputValue(""); // Clear the input field
       setPriority(""); // Clear the priority field
-    } else {
-      return;
     }
   };
-  const del = (index: number) => {
-    const shouldRemove = window.confirm("are you sure you want to delete?");
+
+  const handleTodoPriorityChange = (id: number, newPriority: string) => {
+    setTodoList(
+      todoList.map((todo) =>
+        todo.id === id ? { ...todo, priority: newPriority } : todo
+      )
+    );
+  };
+
+  const del = (id: number) => {
+    const shouldRemove = window.confirm("Are you sure you want to delete?");
     if (shouldRemove) {
-      const newList = todoList.filter((_, i) => i !== index);
+      const newList = todoList.filter((todo) => todo.id !== id);
       setTodoList(newList);
     } else {
       alert("Not Deleted!");
     }
   };
-  let id = 0;
 
   return (
-    <>
-      <div className="todoList-container">
-        <div className="adder">
-          <h2>Add Tasks here</h2>
-          <form onSubmit={(e) => handleFormSubmit(e)}>
-            <label htmlFor="task"> Task: </label>
-            <input
-              type="text"
-              name="task"
-              id="task"
-              value={inputValue}
-              onChange={handleInputChange}
-            />
-            <select
-              className="priority-dropdown"
-              value={priority}
-              onChange={handlePriorityChange}
-            >
-              <option value="" disabled>
-                Priority
-              </option>
-              <option className="high" value="High">
-                High
-              </option>
-              <option className="medium" value="Medium">
-                Medium
-              </option>
-              <option className="low" value="Low">
-                Low
-              </option>
-            </select>
-            <button className="add-button" type="submit">
-              Add
-            </button>
-          </form>
-        </div>
-        <div className="todos">
-          <h2>Todos</h2>
-          <ul className="todo-list">
-            {todoList.map((todo) => {
-              return (
-                id++,
-                (
-                  <div
-                    className={`task-container task ${todo
-                      .split(":")[0]
-                      .toLowerCase()}`}
-                    key={id - 1}
-                    id={`${id - 1}`}
-                  >
-                    <li>{todo.split(":")[0]}</li>
-                    <li>{todo.split(":")[1]} </li>
-                    <button
-                      className="delete-button"
-                      onClick={(e) => {
-                        e.preventDefault();
-                        del(Number(id - 1));
-                      }}
-                    >
-                      Complete
-                    </button>
-                  </div>
-                )
-              );
-            })}
-          </ul>
-        </div>
+    <div className="todoList-container">
+      <div className="adder">
+        <h2>Add Tasks here</h2>
+        <form onSubmit={handleFormSubmit}>
+          <label htmlFor="task"> Task: </label>
+          <input
+            type="text"
+            name="task"
+            id="task"
+            value={inputValue}
+            onChange={handleInputChange}
+          />
+          <select
+            className="priority-dropdown"
+            value={priority}
+            onChange={handlePriorityChange}
+          >
+            <option value="" disabled>
+              Priority
+            </option>
+            <option className="high" value="High">
+              High
+            </option>
+            <option className="medium" value="Medium">
+              Medium
+            </option>
+            <option className="low" value="Low">
+              Low
+            </option>
+          </select>
+          <button className="add-button" type="submit">
+            Add
+          </button>
+        </form>
       </div>
-    </>
+      <div className="todos">
+        <h2>Todos</h2>
+        <ul className="todo-list">
+          {todoList.map((todo) => (
+            <div
+              className={`task-container task ${todo.priority.toLowerCase()}`}
+              key={todo.id}
+              id={`${todo.id}`}
+            >
+              <li>
+                <select
+                  className="priority-dropdown"
+                  value={todo.priority}
+                  onChange={(e) =>
+                    handleTodoPriorityChange(todo.id, e.target.value)
+                  }
+                >
+                  <option className="high" value="High">
+                    High
+                  </option>
+                  <option className="medium" value="Medium">
+                    Medium
+                  </option>
+                  <option className="low" value="Low">
+                    Low
+                  </option>
+                </select>
+              </li>
+              <li>{todo.task}</li>
+              <button
+                className="delete-button"
+                onClick={(e) => {
+                  e.preventDefault();
+                  del(todo.id);
+                }}
+              >
+                Complete
+              </button>
+            </div>
+          ))}
+        </ul>
+      </div>
+    </div>
   );
 };
 
 export default TodoList;
-
-{
-  /* <form onSubmit={(e) => handleFormSubmit(e)}>
-  <label htmlFor="task"> Task: </label>
-
-  <select
-    className="priority-dropdown"
-    value={NewPriority}
-    onChange={handleNewPriority}
-  >
-    <option value="" disabled>
-    {todo.split(":")[0]}
-    </option>
-    <option className="high" value="High">
-      High
-    </option>
-    <option className="medium" value="Medium">
-      Medium
-    </option>
-    <option className="low" value="Low">
-      Low
-    </option>
-  </select>
-  <button className="add-button" type="submit">
-    Add
-  </button>
-</form>; */
-}
